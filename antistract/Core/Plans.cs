c291c8a769c864758a2b4065e3dfc076f5a1c3a3
LeSnakk <cornelius@diefenthal-ww.de>
2022-06-23 13:33:18 +0200

    Plans class ObservableCollection

--- antistract/Core/Plans.cs
+++ antistract/Core/Plans.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,16 @@ namespace antistract.Core
 {
     public class Plans
     {
-        public Plans(string entryName, string title, string type, string duration)
+
+        public ObservableCollection<Entries> Entries;
+
+        public Plans()
+        {
+            Entries = new ObservableCollection<Entries>();
+        }
+
+
+        /*public Plans(string entryName, string title, string type, string duration)
         {
             this.entryName = entryName;
             this.title = title;
@@ -19,6 +29,42 @@ namespace antistract.Core
         public string entryName { get; set; }
         public string title { get; set; }
         public string type { get; set; }
-        public string duration { get; set; }
+        public string duration { get; set; }*/
+    }
+}
+
+public class Entries 
+{
+    public Entries()
+    {
+
+    }
+
+    private string _entryName;
+    public string entryName
+    {
+        get { return _entryName; }
+        set { _entryName = value; }
+    }
+
+    private string _title;
+    public string title
+    {
+        get { return _title; }
+        set { _title = value; }
+    }
+
+    private string _type;
+    public string type
+    {
+        get { return _type; }
+        set { _type = value; }
+    }
+
+    private string _duration;
+    public string duration
+    {
+        get { return _duration; }
+        set { _duration = value; }
     }
 }

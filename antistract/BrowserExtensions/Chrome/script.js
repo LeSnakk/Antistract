efd03968cdbab5958e4afeb8cc3f18654c810a27
Cornelius Diefenthal <cornelius@diefenthal-ww.de>
2022-10-07 16:02:38 +0200

    GCEx: Remove website content

--- antistract/BrowserExtensions/Chrome/script.js
+++ antistract/BrowserExtensions/Chrome/script.js
@@ -1,2 +1,7 @@
+switch (window.location.hostname) {
+    case "www.youtube.com":
+        console.log("test");
+        document.body.innerHTML = "<h1>Oops! Hope nobody saw that. Quick, back to work!</h1>";
+        break;
+}
 console.log("hello world 2");
-alert("hello world");

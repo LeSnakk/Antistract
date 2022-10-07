3bfcb9da8263a64c6638ce4fd72ecf288a15956d
Cornelius Diefenthal <cornelius@diefenthal-ww.de>
2022-10-07 18:52:19 +0200

    GCEx: Obtain checkMode

--- antistract/BrowserExtensions/Chrome/background.js
+++ antistract/BrowserExtensions/Chrome/background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("This alert is coming from the backgroundService!");
+    LoadBlockedWebsites();
 })
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
@@ -13,8 +13,38 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     }
 });
 
-LogCurrentTabs();
 
+
+    LogCurrentTabs();
+
+function LoadBlockedWebsites() {
+    fetch('data.xml').then(r => r.text()).then(result => {
+        ProcessXMLData(result);
+    })
+}
+
+function ProcessXMLData(data) {
+    var xmlData = data;
+    var xmlStringData = String(xmlData);
+    console.log(xmlStringData);
+
+    var checkModeDetermineStart = "<checkMode>";
+    var checkModeDetermineEnd = "</checkMode";
+    var websitesDetermineStart = "<website>";
+    var websitesDetermineEnd = "</website>";
+
+    var checkMode = xmlStringData.split(checkModeDetermineStart).pop().split(checkModeDetermineEnd)[0];
+    var websites = [];
+    
+    console.log(checkMode);
+}
+
+
+
+
+
+
+//DEBUG AREA
 function LogCurrentTabs() {
     chrome.tabs.query({}, (tabs) => { console.log(tabs) });
 }
